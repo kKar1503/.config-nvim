d4b64edba0902fef7fb2dbb265a4819082ea241d
Yam Kar Lok <isonice4572@gmail.com>
2023-07-09 12:54:16 +0800

    Added treesitter-context

--- lua/kar/packer.lua
+++ lua/kar/packer.lua
@@ -20,6 +20,8 @@ return require('packer').startup(function(use)
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
+  use('nvim-treesitter/nvim-treesitter-context')
+
   use('theprimeagen/harpoon')
 
   use('mbbill/undotree')
